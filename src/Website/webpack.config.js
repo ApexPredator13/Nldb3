99621de78300a520684bc1737f9240a8b99323ee
Thomas Mosbacher <thomas.mosbacher@hotmail.com>
2019-04-14 20:09:39 +0200

    webpack and typescript setup

---
+++ src/Website/webpack.config.js
@@ -0,0 +1,32 @@
+﻿const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        validation: './wwwroot/js/src/validation.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'wwwroot', 'js', 'dist'),
+        filename: '[name].[hash].min.js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    }
+                ],
+                exclude: '/node-modules/'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.ts', '.css' ]
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+}
---
+++ src/Website/wwwroot/js/src/validation.ts
@@ -0,0 +1,17 @@
+﻿// just some test code to test webpack bundling
+
+const logSomething = () => console.log('something')
+
+const newDiv = document.createElement('div');
+if (newDiv) {
+    newDiv.innerText = "some text";
+    if (document.firstChild) {
+        document.firstChild.appendChild(newDiv);
+    }
+}
+
+const asyncFunction = async () => {
+    await Promise.resolve();
+}
+
+let x = asyncFunction();
