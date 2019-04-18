43c88abfc9d99f0867b674ad7a46f2e64d2a26cc
Thomas Mosbacher <thomas.mosbacher@hotmail.com>
2019-04-18 21:53:06 +0200

    rewrote validation.ts to make better use of native asp.net core model validation

--- src/Website/wwwroot/js/src/validation.ts
+++ src/Website/wwwroot/js/src/validation.ts
@@ -1,140 +1,86 @@
-﻿import { elementClosest } from './polyfills'
+﻿import { elementClosest } from './polyfills';
 elementClosest();
 
 const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-const validatableInputElements = ["text", "password"];
+const inputElementTypeWhitelist: Array<string | null> = ["text", "password"];
 
-enum ValidationResult {
-    OK = 1,
-    Required,
-    InvalidEmail
+const applyErrormessage = (inputElement: HTMLInputElement, message: string | null): void => {
+    const errorMessageContainer = inputElement.nextElementSibling;
+    if (errorMessageContainer) {
+        errorMessageContainer.textContent = message;
+    }
 }
 
-const validationRules = new Array<(element: HTMLInputElement) => ValidationResult>(
-    e => e.hasAttribute("data-required") && !e.value ? ValidationResult.Required : ValidationResult.OK,
-    e => e.hasAttribute("data-email") && !emailRegex.test(e.value) ? ValidationResult.InvalidEmail : ValidationResult.OK
-)
-
-// finds all input elements that should be validated
-const findInputElements = (someElement: HTMLElement): Array<HTMLInputElement> => {
-    const inputElements = someElement.getElementsByTagName("input");
-    const result = new Array<HTMLInputElement>();
-    if (inputElements && inputElements.length) {
-        for (let i = 0; i < inputElements.length; i++) {
-            const inputType = inputElements[i].getAttribute("type");
-            if (validatableInputElements.indexOf(inputType ? inputType : '') === -1) {
-                continue;
-            }
-            result.push(inputElements[i]);
+const applyFormValidState = (form: HTMLFormElement, isValid: boolean): void => {
+    const submitButton = form.getElementsByTagName("button");
+    for (let i = 0; i < submitButton.length; i++) {
+        if (submitButton[i].getAttribute("type") === "submit") {
+            (<HTMLButtonElement>submitButton[i]).disabled = !isValid;
         }
     }
-    
-    return result;
 }
 
-// takes an input element, validates the form it's part of, and displays the error message next to the form field
-const validateForm = (element: HTMLInputElement, errorClassname: string): void => {
-    let validationResult = checkInputElementRules(element);
-    let formValid = true;
+const inputElementIsValid = (element: HTMLInputElement, target: HTMLInputElement, markAsTouched: boolean): boolean => {
+    let isValid = true;
+    let errorMessage: string | null = null;
+    const isSameNode = element === target;
 
-    const parentForm = element.closest("form");
-    if (!parentForm) {
+    // validation rules
+    if (element.hasAttribute("data-val-required") && !element.value) {
+        isValid = false;
+        errorMessage = errorMessage ? errorMessage : (element.getAttribute("data-val-required") ? element.getAttribute("data-val-required") as string : null);
+    }
+    if(element.hasAttribute("data-val-email") && !emailRegex.test(element.value)) {
+        isValid = false;
+        errorMessage = errorMessage ? errorMessage : (element.getAttribute("data-val-email") ? element.getAttribute("data-val-email") as string : null);
+    }
+    if (element.hasAttribute("data-val-length-max") && element.value.length > parseInt(element.getAttribute("data-val-length-max") as string, 10)) {
+        isValid = false;
+        errorMessage = errorMessage ? errorMessage : (element.getAttribute("data-val-length") ? element.getAttribute("data-val-length") as string : null);
+    }
+    if (element.hasAttribute("data-val-length-min") && element.value.length < parseInt(element.getAttribute("data-val-length-min") as string, 10)) {
+        isValid = false;
+        errorMessage = errorMessage ? errorMessage : (element.getAttribute("data-val-length") ? element.getAttribute("data-val-length") as string : null);
+    }
+
+    if (markAsTouched && isSameNode) {
+        element.setAttribute("touched", "true");
+    }
+
+    if (element.getAttribute("touched") === "true" && isSameNode) {
+        applyErrormessage(element, errorMessage);
+    }
+    return isValid;
+}
+
+const evaluateForm = (target: HTMLInputElement, markAsTouched: boolean): void => {
+    const form = target.closest("form");
+    if (!form) {
         return;
     }
 
-    // 
-    findInputElements(parentForm).map(element => {
-        const valid = checkInputElementRules(element);
-        if (valid !== ValidationResult.OK) {
-            formValid = false;
-        }
-    });
-
-    if (formValid) {
-        disableSubmitButton(parentForm, false);
-    } else {
-        disableSubmitButton(parentForm, true);
-    }
-
-    const sibling = element.nextElementSibling;
-
-    // if OK, remove error message from input element
-    if (validationResult === ValidationResult.OK) {
-        if (sibling && sibling.classList.contains("error-message")) {
-            sibling.textContent = '';
-        }
-        if (element.classList.contains(errorClassname)) {
-            element.classList.remove(errorClassname);
-        }
-
-    // otherwise display error message
-    } else {
-        if (element.hasAttribute("touched")) {
-            // error message
-            if (sibling && sibling.classList.contains("error-message")) {
-                switch (validationResult) {
-                    case ValidationResult.Required:
-                        if (element.hasAttribute("data-val-required")) {
-                            const errorMessage = element.getAttribute("data-val-required");
-                            sibling.textContent = errorMessage ? errorMessage : 'This field is required';
-                        }
-                        break;
-                }
-            }
-            // error class
-            if (!element.classList.contains(errorClassname)) {
-                element.classList.add(errorClassname);
+    let formIsValid = true;
+    const inputElements = form.getElementsByTagName("input");
+    for (let i = 0; i < inputElements.length; i++) {
+        const element = inputElements[i];
+        const type = element.getAttribute("type");
+        if (element.getAttribute("data-val") === "true" && inputElementTypeWhitelist.indexOf(type) !== -1) {
+            if (!inputElementIsValid(element, target, markAsTouched)) {
+                formIsValid = false;
             }
         }
     }
+
+    applyFormValidState(form, formIsValid);
 }
 
-let checkInputElementRules = (element: HTMLInputElement): ValidationResult => {
-    let elementValid = ValidationResult.OK;
-    validationRules.map(test => {
-        const result = test(element)
-        if (result !== ValidationResult.OK) {
-            elementValid = result;
-        }
-    });
-
-    return elementValid;
-}
-
-// disables or enables the submit button of a form
-let disableSubmitButton = (form: HTMLFormElement, disabled: boolean): void => {
-    const buttons = form.getElementsByTagName("button");
-    if (buttons && buttons.length) {
-        for (let i = 0; i < buttons.length; i++) {
-            let buttonType = buttons[i].getAttribute("type");
-            if (buttonType && buttonType.toLowerCase() === "submit") {
-                buttons[i].disabled = disabled;
-                break;
-            }
-        }
+(() => {
+    const inputElements = document.getElementsByTagName("input");
+    for (let i = 0; i < inputElements.length; i++) {
+        evaluateForm(inputElements[i], false);
+        (inputElements[i] as HTMLInputElement).addEventListener("input", e => evaluateForm(e.target as HTMLInputElement, true));
+        (inputElements[i] as HTMLInputElement).addEventListener("blur", e => evaluateForm(e.target as HTMLInputElement, true));
+        (inputElements[i] as HTMLInputElement).addEventListener("click", e => evaluateForm(e.target as HTMLInputElement, false));
     }
-}
+})();
 
-// event that fires for input elements
-const inputEvent = (event: Event): void => {
-    if (event.target) {
-        console.log('checking input...')
-        const target = <HTMLInputElement>event.target;
-        if (!target.hasAttribute("touched")) {
-            target.setAttribute("touched", "true");
-        }
-        validateForm(target, "input-error");
-    }
-}
-
-const startValidating = () => {
-    document.addEventListener("DOMContentLoaded", () => {
-        const inputElements = findInputElements(document.body);
-        inputElements.map(e => e.addEventListener("input", inputEvent))
-        inputElements.map(e => e.addEventListener("change", inputEvent))
-        inputElements.map(e => validateForm(e, "input-error"));
-    });
-}
-
-console.log("staring validation!");
-startValidating();
