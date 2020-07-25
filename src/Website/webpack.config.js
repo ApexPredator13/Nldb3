553b2496afd3cc5843e8dbb83deb34d2b5249196
ApexPredator13 <zombieeater13@gmail.com>
2020-07-25 08:19:18 -0500

    improved webpack configuration

--- src/Website/webpack.config.js
+++ src/Website/webpack.config.js
@@ -5,89 +5,94 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const configuration = 'development';
+module.exports = (env, argv) => {
 
-module.exports = {
-    entry: {
-        css_combined: './wwwroot/css/src/_combined.js',
-        aspnetcore_formvalidation: './Ts/Framework/aspnetcore-formvalidation.js',
-        all_remaining_pages: './Ts/Pages/_all-pages.js',
-        all_admin_pages: './Ts/Pages/Admin/_all-admin-pages.js',
-        home: './Ts/Pages/home.js',
-        downloads: './Ts/Pages/downloads.js',
-        episodes: './Ts/Pages/episodes.js',
-        resource_overview: './Ts/Pages/resource-overview.js',
-        episode: './Ts/Pages/episode.js',
-        resource: './Ts/Pages/resource.js',
-        submit_episode: './Ts/Pages/submit-video.js',
-        quotes: './Ts/Pages/quotes.js',
-        submit_problem: './Ts/Pages/submit-problem.js',
-        random_episode: './Ts/Pages/random-episode-generator.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'wwwroot', 'js', 'dist'),
-        filename: '[name].min.js'
-    },
-    resolve: {
-        extensions: [ '.js' ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new webpack.NormalModuleReplacementPlugin(
-            /Framework[\//]Customizable[\//]config.development.js/,
-            configuration === 'production' ? './config.production.js' : './config.development.js'
-        ),
-        new MiniCssExtractPlugin({
-                filename: '[name].css',
-        }),
-        new OptimizeCssAssetsPlugin(),
-        new CopyPlugin([
-            { 
-                from: path.resolve(__dirname, 'node_modules', 'oidc-client', 'dist', 'oidc-client.slim.min.js'),
-                to: path.resolve(__dirname, 'wwwroot', 'js', 'oidc-client.slim.min.js')
-            }
-        ])
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            }
-        ]
-    },
-    //devtool: 'inline-source-map',
-    mode: 'production',
-    externals: {
-        moment: 'moment'
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            minSize: 0,
-            minChunks: 2,
-            chunks: 'all',
-            cacheGroups: {
-                default: false,
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                    reuseExistingChunk: false
-                },
-                framework: {
-                    test: /([\\/]Framework[\\/])/,
-                    name: 'framework',
-                    reuseExistingChunk: false
+    if (!argv.mode) {
+        console.warn('WARNING - webpack mode not detected, defaulting to \'production\'.');
+    }
+
+    const mode = argv.mode || 'development';
+    console.log('webpack runs in ' + mode + ' mode.');
+
+    return {
+        entry: {
+            css_combined: './wwwroot/css/src/_combined.js',
+            aspnetcore_formvalidation: './Ts/Framework/aspnetcore-formvalidation.js',
+            all_remaining_pages: './Ts/Pages/_all-pages.js',
+            all_admin_pages: './Ts/Pages/Admin/_all-admin-pages.js',
+            home: './Ts/Pages/home.js',
+            downloads: './Ts/Pages/downloads.js',
+            episodes: './Ts/Pages/episodes.js',
+            resource_overview: './Ts/Pages/resource-overview.js',
+            episode: './Ts/Pages/episode.js',
+            resource: './Ts/Pages/resource.js',
+            submit_episode: './Ts/Pages/submit-video.js',
+            quotes: './Ts/Pages/quotes.js',
+            submit_problem: './Ts/Pages/submit-problem.js',
+            random_episode: './Ts/Pages/random-episode-generator.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'wwwroot', 'js', 'dist'),
+            filename: '[name].min.js'
+        },
+        resolve: {
+            extensions: [ '.js' ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new webpack.NormalModuleReplacementPlugin(
+                /Framework[\//]Customizable[\//]config.development.js/,
+                mode === 'development' ? './config.development.js' : './config.production.js'
+            ),
+            new MiniCssExtractPlugin({
+                    filename: '[name].css',
+            }),
+            new OptimizeCssAssetsPlugin(),
+            new CopyPlugin([
+                { 
+                    from: path.resolve(__dirname, 'node_modules', 'oidc-client', 'dist', 'oidc-client.slim.min.js'),
+                    to: path.resolve(__dirname, 'wwwroot', 'js', 'oidc-client.slim.min.js')
+                }
+            ])
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                    ],
+                }
+            ]
+        },
+        devtool: mode === 'development' ? 'inline-source-map' : false,
+        externals: {
+            moment: 'moment'
+        },
+        optimization: {
+            runtimeChunk: 'single',
+            splitChunks: {
+                minSize: 0,
+                minChunks: 2,
+                chunks: 'all',
+                cacheGroups: {
+                    default: false,
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendor',
+                        reuseExistingChunk: false
+                    },
+                    framework: {
+                        test: /([\\/]Framework[\\/])/,
+                        name: 'framework',
+                        reuseExistingChunk: false
+                    }
                 }
             }
+        },
+        performance: {
+            hints: false
         }
-    },
-    performance: {
-        hints: false
-    },
-    watch: true
+    };
 }
-
