d63ab00ceaf29f11f0c91590460b37a326288713
ApexPredator13 <zombieeater13@gmail.com>
2021-04-11 09:14:33 +0200

    renamed method

--- src/Website/Program.cs
+++ src/Website/Program.cs
@@ -15,10 +15,10 @@ namespace Website
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
