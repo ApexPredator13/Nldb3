af957bcc0f4846e0c79ad228859a27a66defe2d0
ApexPredator13 <zombieeater13@gmail.com>
2021-04-25 17:54:00 +0200

    migrations now run before the host runs, making sure the db is up to date before anything happens

--- src/Website/Program.cs
+++ src/Website/Program.cs
@@ -5,9 +5,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Website.Data;
 
 namespace Website
 {
@@ -15,7 +18,15 @@ namespace Website
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
