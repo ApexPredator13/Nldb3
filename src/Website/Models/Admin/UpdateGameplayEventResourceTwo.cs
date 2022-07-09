4991e9dddd2318f2ebc48bf4cb4a78bf5b6b7b5d
ApexPredator13 <zombieeater13@gmail.com>
2022-07-09 19:51:43 +0200

    model for model binding

---
+++ src/Website/Models/Admin/UpdateGameplayEventResourceTwo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Website.Models.Admin
+{
+    public class UpdateGameplayEventResourceTwo
+    {
+        [Required]
+        public int? EventId { get; set; }
+
+        [Required]
+        public string? NewResourceTwo { get; set; }
+    }
+}
